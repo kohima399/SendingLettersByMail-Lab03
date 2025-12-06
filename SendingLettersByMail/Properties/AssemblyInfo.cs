1bf07b978609723e45bed73a40c2a501893c43e5
Илья Худяков <ikingagario@gmail.com>
2025-12-06 23:01:47 +0300

    Добавьте файлы проекта.

---
+++ SendingLettersByMail/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("SendingLettersByMail")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SendingLettersByMail")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d2f133a7-88fe-4fc8-9534-98c12fc74201")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SendingLettersByMail/UsersClasses/SendingEmail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+
+namespace SendingLettersByMail.UsersClasses
+{
+    private class SendingEmail
+    {
+        private InfoEmailSending InfoEmailSending { get; set; }
+
+        private SendingEmail(InfoEmailSending infoEmailSending)
+        {
+            InfoEmailSending = infoEmailSending
+                ?? throw new ArgumentNullException(nameof(infoEmailSending));
+        }
+
+        public void Send()
+        {
+
+        }
+    }
+}
